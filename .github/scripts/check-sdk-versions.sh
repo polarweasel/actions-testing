69bbf313900f1037707be10eb465054bdfd42a7b
Alex Ball <alex.ball@pubnub.com>
2020-06-19 16:57:02 -0700

    generalize, and introduce an error

--- .github/scripts/check-sdk-versions.sh
+++ .github/scripts/check-sdk-versions.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-SDK_LIST=('javascript' 'swift' 'python')
+SDK_LIST=('javascript' 'swift' 'foobarxyz' 'python')
+
+ERROR_FLAG=0
 
 for SDK in "${SDK_LIST[@]}"
 do
-    SDK_VERSION=(`curl -s https://raw.githubusercontent.com/pubnub/$SDK/master/.pubnub.yml | grep ^version | cut -s -d : -f 2 | sed 's/"//g'`)
-    echo $SDK version is $SDK_VERSION
+    SDK_VERSION=$(curl -s -f https://raw.githubusercontent.com/pubnub/$SDK/master/.pubnub.yml | grep ^version | cut -s -d : -f 2 | sed 's/"//g')
+
+    if [ "$SDK_VERSION" == '' ]
+    then
+        echo $SDK: ERROR
+        let ERROR_FLAG=1
+    else
+        echo $SDK: $SDK_VERSION
+    fi
 done
+
+if [ "$ERROR_FLAG" == 1 ]
+then
+    exit 1
+fi
