b1942fa04d2071e37dc2b41880f2245a954b3b20
Alex Ball <alex.ball@pubnub.com>
2020-06-19 17:07:48 -0700

    use proper boolean status flag

--- .github/scripts/check-sdk-versions.sh
+++ .github/scripts/check-sdk-versions.sh
@@ -2,22 +2,20 @@
 
 SDK_LIST=('javascript' 'swift' 'foobarxyz' 'python')
 
-ERROR_FLAG=0
+ERROR_FLAG=false
 
 for SDK in "${SDK_LIST[@]}"
 do
     SDK_VERSION=$(curl -s -f https://raw.githubusercontent.com/pubnub/$SDK/master/.pubnub.yml | grep ^version | cut -s -d : -f 2 | sed 's/"//g')
 
-    if [ "$SDK_VERSION" == '' ]
-    then
+    if [ "$SDK_VERSION" == '' ]; then
         echo $SDK: ERROR
-        let ERROR_FLAG=1
+        ERROR_FLAG=true
     else
         echo $SDK: $SDK_VERSION
     fi
 done
 
-if [ "$ERROR_FLAG" == 1 ]
-then
+if [ "$ERROR_FLAG" = true ]; then
     exit 1
 fi
