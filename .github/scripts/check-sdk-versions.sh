94789930c9de04f578a6b7fc5f6d9b1133a199c1
Alex Ball <alex.ball@pubnub.com>
2020-06-19 18:11:20 -0700

    sort output, update and annotate script

--- .github/scripts/check-sdk-versions.sh
+++ .github/scripts/check-sdk-versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SDK_LIST=('javascript' 'swift' 'foobarxyz' 'python')
+SDK_LIST=('javascript' 'swift' 'php' 'python' 'java' 'dart' 'kotlin' 'unity' 'go' 'c-sharp' 'objective-c' 'ruby' 'c-core' )
 
 ERROR_FLAG=false
 
@@ -9,10 +9,19 @@ do
     SDK_VERSION=$(curl -s -f https://raw.githubusercontent.com/pubnub/$SDK/master/.pubnub.yml | grep ^version | cut -s -d : -f 2 | sed 's/"//g')
 
     if [ "$SDK_VERSION" == '' ]; then
-        echo $SDK: ERROR
+        echo $SDK ERROR
         ERROR_FLAG=true
     else
         echo $SDK: $SDK_VERSION
+        # Instead of an echo, this should actually populate a DB entry
+        # or call a script to replace a value in a file.
+        # 
+        # Something like this:
+        #
+        # sed -i '' "s/^$SDK.*$/$SDK: $SDK_VERSION/g" sdk-versions.txt
+        #
+        # (Note: the -i flag on linux is slightly different than on macOS.)
+        # (Should also test for existence. If it's a new SDK, just append to the file.)
     fi
 done
 
